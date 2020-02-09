c016a293215e71930e57b59c61df919853f64b7e
andreysirak <andreysirak@yahoo.com>
2020-02-09 09:43:35 +0200

    moving git root folder

---
+++ src/main/java/com/example/demo2_angular/Demo2AngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo2_angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2AngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2AngularApplication.class, args);
+    }
+
+}
